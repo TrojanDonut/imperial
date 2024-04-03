3e3b87097b598d08ba83224b26989ae7fdacfe6b
TrojanDonut <tevko2.sedmak@gmail.com>
2024-04-03 10:47:44 +0200

    init commit

--- ui/src/response.ts
+++
@@ -1,6 +0,0 @@
-import { DevelopmentCardDeck } from "../tsg";
-
-export type RPlayerHandCards = { [key: number]: number | undefined };
-export type RPlayerHandDevelopmentCards = {
-    [key: number]: DevelopmentCardDeck | undefined;
-};
